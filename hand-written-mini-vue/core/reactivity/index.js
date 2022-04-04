2d7d9b1365090d377bb67aea5d97bc646d0b67ab
moxunjingmu <1786471868@qq.com>
2022-04-04 23:09:59 +0800

    feat：完成对象收集依赖

--- hand-written-mini-vue/core/reactivity/index.js
+++ hand-written-mini-vue/core/reactivity/index.js
@@ -40,12 +40,52 @@ function effectWatch(effect) {
   currentEffect = null
 }
 
-const dep = new Dep(1)
-let b
-effectWatch(() => {
-  b = dep.value + 10
-  console.log("b", b);
+// 监听对象 ->将对象的key建立一个对应的map
+// 1.监听对象什么时候改变了
+
+// vue2 -> Object.defineProperties
+  // 属性需要一个个执行
+// vue3 -> Proxy
+  //一次代理所有属性
+const targetMap = new Map()
+
+function reactive (raw){
+  return new Proxy(raw, {
+    get(target, key) {
+      console.log("key", key);
+      // dep存储
+      let depsMap = targetMap.get(target)
+
+      // 第一次取可能为undefined，先初始化
+      if(!depsMap){
+        depsMap = new Map()
+        targetMap.set(target, depsMap)
+      }
+      console.log("depsmap", depsMap);
+      let dep = depsMap.get(target, key)
+      if(!dep) {
+        dep = new Dep();
+        depsMap.set(key, dep)
+      }
+
+      // 依赖收集
+      dep.depend()
+      
+      return Reflect.get(target, key)
+    },
+    set(target, key) {
+
+    }
+
+  })
+}
+
+const user = reactive({
+  age: 18
 })
 
-//值发生变化 -》再次调用effect
-dep.value = 2
+effectWatch(() => {
+  user.age
+})
+
+user.age
